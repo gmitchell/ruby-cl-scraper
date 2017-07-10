d74f967aa6e5a7be24b8979139988990155eb107
Greg <01greg@gmail.com>
2017-07-10 08:21:12 -0400

    minor changes

--- craigslist_condo_scaper.rb
+++ craigslist_condo_scaper.rb
@@ -9,8 +9,8 @@ cities.each do |city|
 
     url = "https://#{city}.craigslist.org/search/apa?query=#{term}&postedToday=1&min_price=900&max_price=1500&min_bedrooms=2&availabilityMode=0&pets_dog=1"
 
-    document = open(url)
-    content = document.read
+    document       = open(url)
+    content        = document.read
     parsed_content = Nokogiri::HTML(content)
 
     puts "\n===================================================================="
@@ -20,19 +20,19 @@ cities.each do |city|
     puts "===================================================================="
 
     parsed_content.css('.content').css('.result-row').each do |row|
-    title        = row.css('.hdrlnk').inner_text
-    link         = row.css('.hdrlnk').first.attributes["href"].value
-    posted_at    = row.css('time').first.attributes["datetime"].value
-    price        = row.css('.result-meta').css('.result-price').inner_text
-    neighborhood = row.css('.result-meta').css('.result-hood').inner_text
+      title        = row.css('.hdrlnk').inner_text
+      link         = row.css('.hdrlnk').first.attributes["href"].value
+      posted_at    = row.css('time').first.attributes["datetime"].value
+      price        = row.css('.result-meta').css('.result-price').inner_text
+      neighborhood = row.css('.result-meta').css('.result-hood').inner_text
 
-    puts "Title: #{title} - #{neighborhood}"
-    puts "Price: #{price}"
-    puts "Posted at #{posted_at}"
-    puts link
-    puts "\n"
-    puts "-----------------------------------------------------------------\n"
-    puts "\n"
+      puts "Title: #{title} - #{neighborhood}"
+      puts "Price: #{price}"
+      puts "Posted at #{posted_at}"
+      puts link
+      puts "\n"
+      puts "-----------------------------------------------------------------\n"
+      puts "\n"
     end
   end
 end
